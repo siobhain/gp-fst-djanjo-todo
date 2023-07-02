cd5ab98742d926c010d0aefd7f2ba16ccfbbdfe1
siobhain <sobrien@live.ie>
2023-07-02 03:50:20 +0000

    add_template half done

--- myapptodo/views.py
+++ myapptodo/views.py
@@ -10,3 +10,7 @@ def get_todo_list(request):
         'items': items
     }
     return render(request, 'myapptodo/todo_template.html', context)
+
+
+def add_item(request):
+    return render(request, 'myapptodo/add_template.html')
