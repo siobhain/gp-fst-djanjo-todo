b0faa1bc61ebdae7e6789d9084cf093089f2c555
siobhain <sobrien@live.ie>
2023-07-02 03:19:37 +0000

    add for loop to display all items on home

--- django_todo_project/settings.py
+++ django_todo_project/settings.py
@@ -15,7 +15,8 @@ import os
 
 if os.path.isfile("env.py"):
     import env
-    
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
--- myapptodo/views.py
+++ myapptodo/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 
+
 def get_todo_list(request):
-    return render(request, 'myapptodo/todo_template.html')
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'myapptodo/todo_template.html', context)
