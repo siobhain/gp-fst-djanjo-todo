3f8c2c80a2bc8b3f1f7a76aeaf556824f3c559bb
siobhain <sobrien@live.ie>
2023-07-02 20:43:37 +0000

    include Django Forms, forms.py Change HTML & views

--- myapptodo/views.py
+++ myapptodo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
+from .forms import ItemForm
 
 # Create your views here.
 
@@ -11,11 +12,25 @@ def get_todo_list(request):
     }
     return render(request, 'myapptodo/todo_template.html', context)
 
+# This was how my own tiny form was handled before forms.py imported
+# def add_item(request):
+#     if request.method == 'POST':
+#         name = request.POST.get('item_description')
+#         done = 'done' in request.POST
+#         Item.objects.create(name=name, done=done)
+#         return redirect('get_todo_list')
+#     return render(request, 'myapptodo/add_template.html')
+
 
 def add_item(request):
     if request.method == 'POST':
-        name = request.POST.get('item_description')
-        done = 'done' in request.POST
-        Item.objects.create(name=name, done=done)
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
         return redirect('get_todo_list')
-    return render(request, 'myapptodo/add_template.html')
+    form = ItemForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'myapptodo/add_template.html', context)
